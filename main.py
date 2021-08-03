805a22af6ab8badd8f229f4228d4f620d95f692f
mohammad8017 <mhmd.ghfz@gmail.com>
2021-08-03 16:23:09 +0430

    normalization and features list added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from os import sep
+from numpy.core.numeric import normalize_axis_tuple
 import pandas
 import matplotlib.pyplot as plt
 import numpy as np
@@ -14,11 +15,25 @@ def findLower_Upper(data:list):
 
     return lower_bound, upper_bound
 
+def normalize(data):
+    return [((item - min(data)) / ((max(data)-min(data)))) for item in data]
+
+
 
 
 data = pandas.read_csv('ADANIPORTS.csv')
 
 date, lastClose, open, high, low, last, close, volume = data['Date'], data['Prev Close'], data['Open'], data['High'], data['Low'], data['Last'], data['Close'], data['Volume']
+
+#normalize values
+lastClose = normalize(lastClose)
+open = normalize(open)
+high = normalize(high)
+low = normalize(low)
+last = normalize(last)
+close = normalize(close)
+volume = normalize(volume)
+
 
 plt.subplot(1,3,1)
 plt.plot(open)
@@ -34,3 +49,25 @@ plt.plot(close)
 plt.title('close')
 
 plt.show()
+
+features = []
+for i in range(len(date)):
+    tempList = []
+    tempList.append(lastClose[i])
+    tempList.append(high[i])
+    tempList.append(low[i])
+    tempList.append(last[i])
+    tempList.append(close[i])
+    if close[i] - lastClose[i] >= 0:
+        tempList.append('Positive')
+    else:
+        tempList.append('Negative')    
+    features.append(tempList) 
+
+print(features)       
+
+
+
+
+
+    
